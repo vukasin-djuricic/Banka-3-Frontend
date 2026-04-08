6cd49aa2c5e8227fc086b4ad4c3836ced5032683
lazar125 <lmirkovic12522rn@raf.rs>
2026-04-08 15:34:03 +0200

    card masking fix

--- src/services/CardService.js
+++ src/services/CardService.js
@@ -33,13 +33,9 @@ export async function getUserAccounts() {
 export async function requestCard(cardData) {
   const response = await api.post("/cards", {
     account_number: cardData.accountNumber,
-<<<<<<< HEAD
-    card_type: "debit",
-    card_brand: "visa"
-=======
+
     card_type: "DEBIT",
     card_brand: "VISA",
->>>>>>> aded30f (card masking fix)
   });
 
   return response.data;
